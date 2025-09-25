eb109bc7237a7cf676868cf51f0e199d87ea074e
kannant11 <kannant@merrimack.edu>
2025-09-25 19:09:14 -0400

    created a mail queue as a linked blocking queue [created a constructor to create the queue, created a getter to receive that queue]

--- src/smtpserver/MailQueue.java
+++ src/smtpserver/MailQueue.java
@@ -1,5 +1,19 @@
 package smtpserver;
 
+import java.util.concurrent.LinkedBlockingQueue;
+
 public class MailQueue {
+
+    private LinkedBlockingQueue<String> mailQueue;
     
+    //constructor creating a linked blocking queue
+    public MailQueue() {
+        //creates a linked blocking queue
+        this.mailQueue = new LinkedBlockingQueue<>();
+    }
+
+    //getter receiving the linked blocking queue
+    public LinkedBlockingQueue<String> getLinkedBlockingQueue() {
+        return mailQueue;
+    }
 }
